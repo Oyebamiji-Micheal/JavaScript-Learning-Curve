a62f28b07184ceec115bcf24702dcd4feaa4461e
Oyebamiji Micheal Tomiwa <oyebamijimicheal10@gmail.com>
2023-03-01 23:14:46 +0100

    Create arrays.js

---
+++ arrays.js
@@ -0,0 +1,21 @@
+// Arrays
+
+// Defining an array
+const courses = ['Algebra', 'Mechanics', 'Statistics', 'Calulus']
+console.log(courses)
+
+// Common array methods
+
+// Adding an element to the end of the array
+courses.push('Geometry')
+console.log(courses)
+
+// Array Slicing
+console.log(courses.slice(2, ))
+
+// Getting the index of an element in the array
+console.log(courses.indexOf('Mechanics'))
+
+// Number of elements in an array
+console.log(`There are ${courses.length} elements in the array`)
+
