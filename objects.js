3f3bb813f8db78d5d08f1688f60b4d062602b955
Oyebamiji Micheal Tomiwa <oyebamijimicheal10@gmail.com>
2023-03-02 23:35:54 +0100

    Create objects.js

---
+++ objects.js
@@ -0,0 +1,23 @@
+// OBJECTS
+
+// Defining objects
+const programmer = {
+    name: 'Smith',
+    age: 21,
+    stack: 'Web Developer'
+}
+console.log(programmer)
+
+
+// Accessing object properties
+console.log(programmer.name)
+// console.log(programmer['name'])
+
+console.log(programmer.stack)
+// console.log(programmer['stack'])
+
+
+// Adding a new property to an object
+programmer['level'] = 'Junior'
+console.log(programmer)
+
